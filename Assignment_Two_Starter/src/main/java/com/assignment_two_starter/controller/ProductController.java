9c2a491faa4db9f6b56e0120e0ed1280aa992fa6
eoinhasty <145497131+eoinhasty@users.noreply.github.com>
2024-11-12 13:53:18 +0000

    Add Backend endpoints for getAllProducts, getProductById and get ProductImages

--- Assignment_Two_Starter/src/main/java/com/assignment_two_starter/controller/ProductController.java
+++ Assignment_Two_Starter/src/main/java/com/assignment_two_starter/controller/ProductController.java
@@ -1,18 +1,51 @@
 package com.assignment_two_starter.controller;
 
+import com.assignment_two_starter.model.entities.Product;
 import com.assignment_two_starter.model.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.io.File;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/products")
 public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @PostMapping("/")
-    public ResponseEntity getAllProducts()
+    @GetMapping("/")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        if (products == null || products.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<Product> getProductById(@PathVariable Integer productId) {
+        Product product = productService.getProductById(productId);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product);
+    }
+
+    @GetMapping("/{productId}/images")
+    public ResponseEntity<List<String>> getProductImages(@PathVariable Integer productId) throws IOException {
+        File directory = new ClassPathResource("static/assets/images/large/" + productId).getFile();
+        FilenameFilter jpgFilter = (dir, name) -> name.toLowerCase().endsWith(".jpg");
+        String[] imageFiles = directory.list(jpgFilter);
+
+        List<String> images = Arrays.asList(imageFiles);
+
+        return ResponseEntity.ok(images);
+    }
 }
