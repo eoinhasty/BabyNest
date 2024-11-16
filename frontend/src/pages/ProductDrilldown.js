400ec6a4909619fa3379e035235d553a6417d152
eoinhasty <145497131+eoinhasty@users.noreply.github.com>
2024-11-16 15:39:37 +0000

    Moved css files to css folder

--- frontend/src/pages/ProductDrilldown.js
+++ frontend/src/pages/ProductDrilldown.js
@@ -2,7 +2,7 @@ import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import '../ProductDrilldown.css';
+import '../css/ProductDrilldown.css';
 
 function ProductDrilldown() {
 
