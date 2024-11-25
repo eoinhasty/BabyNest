acfb00a2d810bb1a80cb3219ee811a299c34634b
eoinhasty <145497131+eoinhasty@users.noreply.github.com>
2024-11-25 13:01:48 +0000

    Updated Navbar to show items based on if user is authenticated or not, Added logout button to remove token from cookies

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,8 +1,30 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, {useEffect} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import Cookies from "js-cookie";
 import '../css/NavBar.css';
 
 function NavBar() {
+    const [loggedIn, setLoggedIn] = React.useState(!!Cookies.get('jwt'));
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setLoggedIn(!!Cookies.get('jwt'));
+    }, []);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setLoggedIn(!!Cookies.get('jwt'));
+        }, 500); // Check every 500ms
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const handleLogout = () => {
+        Cookies.remove('jwt');
+        setLoggedIn(false);
+        navigate('/');
+    };
+
     return (
         <nav className="navbar">
             <div className="navbar-logo">
@@ -13,7 +35,21 @@ function NavBar() {
             <ul className="navbar-links">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/products">Products</Link></li>
-                <li><Link to="/register">Register</Link></li>
+                {
+                    loggedIn ? (
+                        <>
+                            <li><Link to="/cart">Cart {}</Link></li>
+                            <li>
+                                <button onClick={handleLogout} className={"logout-button"}>Logout</button>
+                            </li>
+                        </>
+                    ) : (
+                        <>
+                            <li><Link to="/login">Login</Link></li>
+                            <li><Link to="/register">Register</Link></li>
+                        </>
+                    )
+                }
             </ul>
         </nav>
     );
