5e0b5eb68a388faeb840409133c966267cafdac5
eoinhasty <145497131+eoinhasty@users.noreply.github.com>
2024-11-25 13:02:50 +0000

    Added login and Cart Routes to Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import ProductDrilldown from "./pages/ProductDrilldown";
 import NavBar from "./components/NavBar";
 import Register from "./pages/Register";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
     return (
@@ -15,6 +16,8 @@ function App() {
                     <Route path="/" element={<AllProducts/>}/>
                     <Route path="/products" element={<AllProducts/>}/>
                     <Route path="/products/:productId" element={<ProductDrilldown/>}/>
+                    <Route path={"/cart"} element={<h1>Cart</h1>}/>
+                    <Route path={"/login"} element={<Login/>}/>
                     <Route path={"/register"} element={<Register/>}/>
                     <Route path={"*"} element={<h1>Not Found</h1>}/>
                 </Routes>
