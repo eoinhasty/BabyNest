4df8b29c990681e87d3fb483843245980963701a
eoinhasty <145497131+eoinhasty@users.noreply.github.com>
2024-11-26 16:45:51 +0000

    Created ProtectedRoute component to protect frontend endpoints

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,10 @@ import AllProducts from "./pages/AllProducts";
 import ProductDrilldown from "./pages/ProductDrilldown";
 import NavBar from "./components/NavBar";
 import Register from "./pages/Register";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
     return (
@@ -16,7 +18,12 @@ function App() {
                     <Route path="/" element={<AllProducts/>}/>
                     <Route path="/products" element={<AllProducts/>}/>
                     <Route path="/products/:productId" element={<ProductDrilldown/>}/>
-                    <Route path={"/cart"} element={<h1>Cart</h1>}/>
+                    <Route path={"/cart"} element={
+                        <ProtectedRoute>
+                           <Cart/>
+                        </ProtectedRoute>
+                    }
+                    />
                     <Route path={"/login"} element={<Login/>}/>
                     <Route path={"/register"} element={<Register/>}/>
                     <Route path={"*"} element={<h1>Not Found</h1>}/>
