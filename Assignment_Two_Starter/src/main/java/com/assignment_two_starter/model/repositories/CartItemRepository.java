a5cd7aed07c0f6abce0bdf094d2fae5e5e4aba77
eoinhasty <145497131+eoinhasty@users.noreply.github.com>
2024-12-01 22:21:29 +0000

    Added query to find cartItem by product for removing archived products from shopping carts

--- Assignment_Two_Starter/src/main/java/com/assignment_two_starter/model/repositories/CartItemRepository.java
+++ Assignment_Two_Starter/src/main/java/com/assignment_two_starter/model/repositories/CartItemRepository.java
@@ -1,7 +1,13 @@
 package com.assignment_two_starter.model.repositories;
 
 import com.assignment_two_starter.model.entities.CartItem;
+import com.assignment_two_starter.model.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
+    List<CartItem> findByProduct(Product product);
 }
