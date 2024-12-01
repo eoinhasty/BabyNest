1144952de04ec5f1a5a44d4e3b813516bcd59e46
eoinhasty <145497131+eoinhasty@users.noreply.github.com>
2024-12-01 22:35:49 +0000

    Updated routes to include newer endpoints and protected endpoints

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,13 @@ import NavBar from "./components/NavBar";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Checkout from "./pages/Checkout";
 import ProtectedRoute from "./components/ProtectedRoute";
+import AdminRoute from "./components/AdminRoute";
+import MyOrders from "./pages/MyOrders";
+import OrderDrilldown from "./pages/OrderDrilldown";
+import AdminDashboard from "./pages/AdminDashboard";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
     return (
@@ -22,8 +27,27 @@ function App() {
                         <ProtectedRoute>
                            <Cart/>
                         </ProtectedRoute>
-                    }
-                    />
+                    }/>
+                    <Route path={"/checkout"} element={
+                        <ProtectedRoute>
+                            <Checkout/>
+                        </ProtectedRoute>
+                    }/>
+                    <Route path={"/orders"} element={
+                        <ProtectedRoute>
+                            <MyOrders/>
+                        </ProtectedRoute>
+                    }/>
+                    <Route path={"/orders/:orderId"} element={
+                        <ProtectedRoute>
+                            <OrderDrilldown/>
+                        </ProtectedRoute>
+                    }/>
+                    <Route path={"/admin"} element={
+                        <AdminRoute>
+                            <AdminDashboard/>
+                        </AdminRoute>
+                    }/>
                     <Route path={"/login"} element={<Login/>}/>
                     <Route path={"/register"} element={<Register/>}/>
                     <Route path={"*"} element={<h1>Not Found</h1>}/>
