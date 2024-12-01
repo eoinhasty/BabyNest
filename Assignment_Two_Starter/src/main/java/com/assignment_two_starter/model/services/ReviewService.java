18626f8e14a996a4ec78f7fb7dc9a5d9c338e008
eoinhasty <145497131+eoinhasty@users.noreply.github.com>
2024-12-01 22:28:14 +0000

    Added getPendingReviews, getReviews by productId and approveReview methods

--- Assignment_Two_Starter/src/main/java/com/assignment_two_starter/model/services/ReviewService.java
+++ Assignment_Two_Starter/src/main/java/com/assignment_two_starter/model/services/ReviewService.java
@@ -2,10 +2,12 @@ package com.assignment_two_starter.model.services;
 
 import com.assignment_two_starter.model.entities.Review;
 import com.assignment_two_starter.model.repositories.ReviewRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ReviewService {
     private final ReviewRepository reviewRepository;
 
@@ -19,6 +21,25 @@ public class ReviewService {
             return c.get();
         else
             return null;
+    }
+
+    public List<Review> getPendingReviews() {
+        return reviewRepository.findByApprovedFalseAndCustomer_LockedFalse();
+    }
+
+    public List<Review> getReviewsByProductId(Integer productId) {
+        return reviewRepository.findByProductProductIdAndApprovedTrue(productId);
+    }
+
+    public Review approveReview(Integer reviewId) {
+        Optional<Review> review = reviewRepository.findById(reviewId);
+        if(review.isPresent()) {
+            Review r = review.get();
+            r.setApproved(true);
+            return reviewRepository.save(r);
+        } else {
+            return null;
+        }
     }
 
     public List<Review> getAllReviews() {
