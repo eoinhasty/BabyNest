b2351e26b3e58d700a18f8e7eccd1cad80b54f68
eoinhasty <145497131+eoinhasty@users.noreply.github.com>
2024-12-01 22:29:27 +0000

    Added CRUD methods and methods for updating product quantity and archival methods

--- Assignment_Two_Starter/src/main/java/com/assignment_two_starter/model/services/ProductService.java
+++ Assignment_Two_Starter/src/main/java/com/assignment_two_starter/model/services/ProductService.java
@@ -1,9 +1,14 @@
 package com.assignment_two_starter.model.services;
 
+import com.assignment_two_starter.model.dto.ProductDTO;
 import com.assignment_two_starter.model.entities.Product;
 import com.assignment_two_starter.model.repositories.ProductRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,15 +28,53 @@ public class ProductService {
             return null;
     }
 
-    public List<Product> getAllProducts() {
-        return productRepository.findAll();
+    public List<Product> getUnarchivedProducts() {
+        return productRepository.findByArchivedFalse();
     }
 
     public void createProduct(Product product) {
         productRepository.save(product);
     }
 
-    public void deleteProduct(Product product) {
-        productRepository.delete(product);
+    @Transactional
+    public Product updateProduct(Integer productId, ProductDTO productDTO) {
+        Product product = getProductById(productId);
+        product.setName(productDTO.getName());
+        product.setPrice(productDTO.getPrice());
+        product.setStockQuantity(productDTO.getStockQuantity());
+        product.setDescription(productDTO.getDescription());
+        product.setUpdatedAt(new Date());
+        return productRepository.save(product);
+    }
+
+    public void clearExistingImages(Integer productId) throws IOException {
+        File directory = new File("src/main/resources/static/assets/images/large/" + productId);
+        if (directory.exists()) {
+            for (File file : directory.listFiles()) {
+                file.delete();
+            }
+        }
+    }
+
+    public void updateProductQuantity(Integer productId, int quantity) {
+        Product product = getProductById(productId);
+        product.setStockQuantity(product.getStockQuantity() - quantity);
+        productRepository.save(product);
+    }
+
+    public void archiveProduct(Integer productId) {
+        Product product = getProductById(productId);
+        product.setArchived(true);
+        productRepository.save(product);
+    }
+
+    public void unarchiveProduct(Integer productId) {
+        Product product = getProductById(productId);
+        product.setArchived(false);
+        productRepository.save(product);
+    }
+
+    public List<Product> getArchivedProducts() {
+        return productRepository.findByArchivedTrue();
     }
 }
